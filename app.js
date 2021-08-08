ff6351518d62d18144a857e495d1cbf789746e2a
Ajay <ajaybains2004@gmail.com>
2021-08-08 22:38:07 +0530

    modified connection strings key value

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set("port", process.env.PORT || 5000);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGOURI);
     // app.listen(port, console.log(`server is listening on port ${port}...`));
     app.listen(app.get("port"), function () {
       console.log("Node server is running on port " + app.get("port"));
